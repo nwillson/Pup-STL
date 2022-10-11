9d489ce3903ca74c1d539a7156081a1473356764
nwillson <nwillson111@gmail.com>
2022-10-11 12:12:55 -0500

    final oct 11

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-// 'Import' the http module
-const http = require("http");
+// 'Import' the Express module instead of http
+const express = require("express");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB);
+dotenv.config();
 
+// Initialize the Express application
+const app = express();
+
+mongoose.connect(process.env.MONGODB);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection Error:"));
@@ -12,21 +17,45 @@ db.once(
   console.log.bind(console, "Successfully opened connection to Mongo!")
 );
 
-// Initialize the http server
-const server = http
-  .createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
 
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+app.get("/echo/:input", (request, response) => {
+  const message = request.params.input;
+  response.status(418).json({ echo: message });
+});
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
