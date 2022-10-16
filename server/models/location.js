e2e84dd83a52f00e50ba2a5a6c6679bba71e9978
nwillson <nwillson111@gmail.com>
2022-10-16 13:17:32 -0500

    added new cluster

--- server/models/location.js
+++ server/models/location.js
@@ -21,32 +21,3 @@ const locationSchema = new mongoose.Schema({
 const Location = mongoose.model("Location", locationSchema);
 
 module.exports = Location;
-
-console.log(Location);
-
-const pizzaSchema = new mongoose.Schema({
-  customer: {
-    type: String,
-    required: true,
-    validate: /^[A-Za-z0-9 ]*$/
-  },
-  crust: {
-    type: String,
-    required: true,
-    enum: ["thin", "chicago", "deep-dish", "hella-thick"]
-  },
-  cheese: {
-    type: String,
-    validate: /^[A-Za-z0-9 ]*$/
-  },
-  sauce: {
-    type: String,
-    required: true,
-    validate: /^[A-Za-z0-9 ]*$/
-  },
-  toppings: [String]
-});
-
-const Pizza = mongoose.model("Pizza", pizzaSchema);
-
-module.exports = Pizza;
